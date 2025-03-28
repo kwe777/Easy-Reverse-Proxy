4d063ef85222213958a0a4b8d274be6575db8e3a
kwe777 <feng.kwe@foxmail.com>
2025-03-28 15:19:37 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 export let PROXIES: Proxy[] = [
    {
         domain: "easy-reverse-proxy-phi.vercel.app",
-        url: "https://aistudio.google.com/",
+        url: "https://www.google.com/",
         enable: true
     }
 ];
